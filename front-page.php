ff19486c1028c86edf0ddfa1124e8318065b05ae
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2024-04-09 14:34:43 +0300

    button update

--- front-page.php
+++ front-page.php
@@ -1,5 +1,13 @@
 <?php
 get_header();
-
-
+?>
+<main style="background-color: #fff">
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br>
+</main>
+<?php
 get_footer();
