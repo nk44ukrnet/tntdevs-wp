4850b7512668fefb7d2f5ea53fa37d88c73cb9e5
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2024-04-10 13:35:58 +0300

    theme update

--- front-page.php
+++ front-page.php
@@ -1,13 +1,70 @@
 <?php
 get_header();
 ?>
-<main style="background-color: #fff">
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br>
-</main>
+    <div class="hb-front-hero"
+         style="background-image: url(<?php echo get_template_directory_uri() ?>/img/hero.jpg); background-size: cover;">
+        <div class="hb-front-hero__overlay"></div>
+        <div class="hb-container">
+            <h1 class="hb-front-hero__heading">
+                <span>WordPress Development Services in</span>
+                <br>
+                <strong>IRELAND</strong>
+            </h1>
+            <div class="hb-front-hero__service-boxes">
+                <div class="hb-front-hero__box">
+                    <div class="hb-front-hero__content">
+                        <div class="hb-front-hero__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-briefcase" viewBox="0 0 16 16">
+                                <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5m1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0M1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5"/>
+                            </svg>
+                        </div>
+                        <p class="hb-front-hero__text">Individual Approach</p>
+                    </div>
+                </div>
+                <div class="hb-front-hero__box">
+                    <div class="hb-front-hero__content">
+                        <div class="hb-front-hero__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-clipboard2-data" viewBox="0 0 16 16">
+                                <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z"/>
+                                <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"/>
+                                <path d="M10 7a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0zm-6 4a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0zm4-3a1 1 0 0 0-1 1v3a1 1 0 1 0 2 0V9a1 1 0 0 0-1-1"/>
+                            </svg>
+                        </div>
+                        <p class="hb-front-hero__text">High Quality Service</p>
+                    </div>
+                </div>
+                <div class="hb-front-hero__box">
+                    <div class="hb-front-hero__content">
+                        <div class="hb-front-hero__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-alarm" viewBox="0 0 16 16">
+                                <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9z"/>
+                                <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1zm1.038 3.018a6 6 0 0 1 .924 0 6 6 0 1 1-.924 0M0 3.5c0 .753.333 1.429.86 1.887A8.04 8.04 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5M13.5 1c-.753 0-1.429.333-1.887.86a8.04 8.04 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1"/>
+                            </svg>
+                        </div>
+                        <p class="hb-front-hero__text">Quick Turnaround</p>
+                    </div>
+                </div>
+                <div class="hb-front-hero__box">
+                    <div class="hb-front-hero__content">
+                        <div class="hb-front-hero__icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-compass" viewBox="0 0 16 16">
+                                <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016m6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0"/>
+                                <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+                            </svg>
+                        </div>
+                        <p class="hb-front-hero__text">All Over the Ireland</p>
+                    </div>
+                </div>
+            </div>
+            <div class="hb-front-hero__person-wrapper">
+                <img class="hb-front-hero__person" src="<?php echo get_template_directory_uri() ?>/img/man.png"
+                     alt="man">
+            </div>
+        </div>
+    </div>
 <?php
 get_footer();
--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 window.addEventListener('DOMContentLoaded', function () {
     //hamburger menu
-    console.log('test');
+    try {
+        let navMenuTrigger = document.querySelector('.hb-header__nav-trigger'),
+            headerElement = document.querySelector('.hb-header'),
+            navMenu = document.querySelector('.hb-header__nav'),
+            activeCSSClass = 'nav-active';
+        if (navMenuTrigger && headerElement && activeCSSClass && navMenu) {
+            navMenuTrigger.addEventListener('click', function (e) {
+                e.stopPropagation();
+                headerElement.classList.toggle(activeCSSClass);
+            })
+            navMenu.addEventListener('click', function (e) {
+                e.stopPropagation();
+            })
+            document.body.addEventListener('click', function (e) {
+                let current = e.target;
+                if (!current.closest('.hb-header')) {
+                    headerElement.classList.remove(activeCSSClass);
+                }
+            })
+        }
+    } catch (e) {
+        console.log(e);
+    }
 })
